8a55e3bbae82d1ce13b848decd20cb9dbcb186ca
YarikCool <yarikmaster007@gmail.com>
2021-09-18 20:29:51 +0300

    Update gulp file + added footer

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,19 @@ const scss = require("gulp-sass");
 const rename = require("gulp-rename");
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
+const imageMin = require("gulp-imagemin");
+const htmlMin = require("gulp-htmlmin");
 
 // Static server
 gulp.task("server", function () {
   //delaet func chtoby zapuskat live server(!!na lokalnoy sieti. to est lyuboy device na etoy wi-fi sietu smozhet zayti na seerver po etomu adresu!!)
   browserSync({
     server: {
-      baseDir: "src",
+      baseDir: "dist",
     },
   });
   gulp.watch("src/*.html").on("change", browserSync.reload);
+  gulp.watch("src/js/**/*.js").on("change", browserSync.reload);
 });
 
 gulp.task("styles", function () {
@@ -33,13 +36,54 @@ gulp.task("styles", function () {
         compatibility: "ie8",
       })
     )
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function () {
   // delat tak, chtoby vse faily izmenalis i kompelirovalis v rezhime realnoho vremeni
   gulp.watch("src/scss/**/*.scss", gulp.parallel("styles"));
+  gulp.watch("src/img/**/*", gulp.parallel("images"));
+  gulp.watch("src/icons/**/*", gulp.parallel("icons"));
+  gulp.watch("src/fonts/**/*", gulp.parallel("fonts"));
+  gulp.watch("src/js/**/*.js", gulp.parallel("scripts"));
+  gulp.watch("src/*.html").on("change", gulp.parallel("html"));
 });
 
-gulp.task("default", gulp.parallel("watch", "server", "styles"));
+gulp.task("html", function () {
+  return gulp.src("src/*.html").pipe(
+    htmlMin({
+      collapseWhitespace: true,
+    }).pipe(gulp.dest("dist/"))
+  );
+});
+
+gulp.task("scripts", function () {
+  return gulp.src("src/js/**/*.js").pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("fonts", function () {
+  return gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task("icons", function () {
+  return gulp.src("src/icons/**/*").pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("src/img/**/*").pipe(imageMin()).pipe(gulp.dest("dist/img"));
+});
+
+gulp.task(
+  "default",
+  gulp.parallel(
+    "watch",
+    "server",
+    "styles",
+    "images",
+    "html",
+    "icons",
+    "scripts",
+    "fonts"
+  )
+);
